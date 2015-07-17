19c52958c5bc7cdc2cb530395a56a9278eb8cc45
JackDesBwa <jackdesbwa@gmail.com>
2015-07-18 01:08:24 +0200

    arduino: whaooo, our own heart beats are visible in the noise WTF

--- arduino_leoke/arduino_leoke.ino
+++ arduino_leoke/arduino_leoke.ino
@@ -2,19 +2,35 @@
 
 #define KEYSNB (sizeof(L)/sizeof(*L))
 
+#define PADBIT(PAD) (1 << PAD)
+
 typedef enum {
   UNTOUCHED,
   TOUCHED,
 } touchstatus_e;
 
-touchstatus_e touching[KEYSNB];
+struct binder_s {
+  uint32_t bits;
+  uint32_t mask;
+
+  touchstatus_e touched;
+} binders[] = {
+  { PADBIT(1) | PADBIT(2), 0xffffffff, UNTOUCHED },
+  { PADBIT(5) | PADBIT(6), 0xffffffff, UNTOUCHED },
+};
+
+#define BINDERNB (sizeof(binders)/sizeof(*binders))
 
 void setup() {
   // All others are input and untouched
   for (int i = 0; i < KEYSNB; ++i) {
     pinMode(L[i], INPUT);
     digitalWrite(L[i], LOW);
-    touching[i] = UNTOUCHED;
+  }
+
+  // Reset binders
+  for (int i = 0; i < BINDERNB; ++i) {
+    binders[i].touched = UNTOUCHED;
   }
 
   // Services begin
@@ -31,19 +47,25 @@ void loop() {
     for (int j = i + 1; j < KEYSNB; ++j) {
       bitj <<= 1;
       int sensorValue = digitalRead(L[j]);
-      if (!sensorValue && !touching[j]) {
-        Serial.print("On ");
-        Serial.println(j);
-        pushed |= bitj;
-      } else if (sensorValue && touching[j]) {
-        Serial.print("Off ");
-        Serial.println(j);
-      }
-      touching[j] = (!sensorValue) ? TOUCHED : UNTOUCHED;
+      if (!sensorValue)
+        pushed |= bitj ;
     }
     if (active != pushed) {
       Serial.print("Code : ");
       Serial.println(pushed, HEX);
+    }
+    for (int j = 0; j < BINDERNB; ++j) {
+      binder_s & binder = binders[j];
+      touchstatus_e touched = (binder.bits & binder.mask == pushed & binder.mask) ? TOUCHED : UNTOUCHED;
+      if (touched && binder.touched == UNTOUCHED) {
+        Serial.print("On ");
+        Serial.println(pushed, HEX);
+
+      } else if (!touched && binder.touched == TOUCHED) {
+        Serial.print("Off ");
+        Serial.println(pushed, HEX);
+      }
+      binder.touched = touched;
     }
     active <<= 1;
     pinMode(L[i], INPUT);
